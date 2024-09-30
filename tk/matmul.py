0bcd46f65eb4fae670ce7c710b87171448b3db4b
Daniel Luo <dan.luo1996@gmail.com>
2024-09-30 22:18:36 +0000

    docs: added a lot of examples to the docstrings for get_2d_mask and get_2d_offset

--- tk/matmul.py
+++ tk/matmul.py
@@ -49,13 +49,91 @@ def get_2d_offset(offsets_0, offsets_1, stride_0, stride_1):
           because:
             [2] * 7 + [0, 1, 2] = [14] + [0, 1, 2] = [14 14 14] + [0 1 2] = [14 15 16]
             [3]                   [21]               [21 21 21]   [0 1 2]   [21 22 23]
+    
+    Scenario 4:
+        - `pid_x = 1` which means `offsets_0` is [3, 4, 5], assuming bs_x = 3.
+        - `pid_y = 3` which means `offsets_1` is [6, 7], assuming bs_y = 2.
+        - The combined 2D offset is:
+                        [[27, 28],
+                         [34, 35]
+                         [41, 42]]
+          because:
+            [3]               [21]           [21 21]   [6 7]   [27 28]
+            [4] * 7 + [6 7] = [28] + [6 7] = [28 28] + [6 7] = [34 35]
+            [5]               [35]           [35 35]   [6 7]   [41 42]
     """
-    return tl.expand_dims(offsets_0, axis=1) * stride_0 + tl.expand_dims(offsets_1, axis=0) * stride_1
+    return (
+        tl.expand_dims(offsets_0, axis=1) * stride_0 + 
+        tl.expand_dims(offsets_1, axis=0) * stride_1
+    )
+
 
 @triton.jit
 def get_2d_mask(offsets_0, offsets_1, max_0, max_1):
-    return (tl.expand_dims(offsets_0, axis=1) < max_0) & (tl.expand_dims(offsets_1, axis=0) < max_1)
+    """
+    For example, suppose we have the tensor:
 
+                    pid_y
+             ------------------>
+            |  0  1  2  3  4  5  6
+      pid_x |  7  8  9 10 11 12 13
+            | 14 15 16 17 18 19 20
+            | 21 22 23 24 25 26 27
+    
+    Since the tensor is 4x7, we have `max_0 = 4` and `max_1` = 7.
+    
+    Scenario 1:
+        - `pid_x = 0` which means `offsets_0` is [0, 1], assuming bs_x = 2.
+        - `pid_y = 0` which means `offsets_1` is [0, 1], assuming bs_y = 2.
+        - The 2D mask is:
+                        [[True, True],
+                         [True, True]]
+          because:
+                [0] < 4 & [0, 1] < 7 = [True] + [True True] = [True True]
+                [1]                    [True]                 [True True]
+
+    Scenario 2:
+        - `pid_x = 0` which means `offsets_0` is [0, 1], assuming bs_x = 2.
+        - `pid_y = 3` which means `offsets_1` is [6, 7], assuming bs_y = 2.
+        - The 2D mask is:
+                        [[True, False],
+                         [True, False]]
+            because:
+
+                [0] < 4 & [6, 7] < 7 = [True] + [True, False] = [True True] & [True False] = [True False]
+                [1]                    [True]                   [True True]   [True False]   [True False]
+            
+            This makes sense: we saw earlier that the combined 2D offset is
+                [[ 6,  7],
+                 [13, 14]]
+            but 7 and 14 are out of bounds since there are only 7 columns (not 8).
+    
+    Scenario 3:
+        - `pid_x = 1` which means `offsets_0` is [2, 3], assuming bs_x = 2.
+        - `pid_y = 0` which means `offsets_1` is [0, 1, 2], assuming bs_y = 3.
+        - The 2D mask is:
+                        [[True, True, True],
+                         [True, True, True]]
+          because:
+            [2] < 4 & [0, 1, 2] < 7 = [True] + [True, True, True] = [True True True]
+            [3]                       [True]                        [True True True]
+    
+    Scenario 4:
+        - `pid_x = 1` which means `offsets_0` is [3, 4, 5], assuming bs_x = 3.
+        - `pid_y = 3` which means `offsets_1` is [6, 7], assuming bs_y = 2.
+        - The 2D mask is:
+                        [[True, False],
+                         [False, False]
+                         [False, False]]
+          because:
+            [3]                   [True]                    [True  True]    [True False]   [True False]
+            [4] < 4 & [6 7] < 7 = [False] & [True, False] = [False False] & [True False] = [False False]
+            [5]                   [False]                   [False False]   [True False]   [False False]
+    """
+    return (
+        (tl.expand_dims(offsets_0, axis=1) < max_0) & 
+        (tl.expand_dims(offsets_1, axis=0) < max_1)
+    )
 
 @triton.jit
 def kernel_matmul_naive(
