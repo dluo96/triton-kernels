5269dcab8ed71b8f1201bb2283ec7bf65a7714de
Daniel Luo <dan.luo1996@gmail.com>
2024-10-08 14:20:35 +0000

    refactor: switch to pytest for swizzle test

--- tk/swizzle_test.py
+++ tk/swizzle_test.py
@@ -1,34 +1,71 @@
-import unittest
-
+import pytest
 import torch
 
 from tk.swizzle import kernel_swizzle
 
 
-class TestSwizzle(unittest.TestCase):
-    def test_swizzle(self):
-        num_blocks_m = 5
-        num_blocks_n = 4
-        x = torch.arange(num_blocks_m * num_blocks_n, device="cuda").view(
-            num_blocks_m, num_blocks_n
-        )
-        z = -torch.ones_like(x)
-        kernel_swizzle[(num_blocks_m, num_blocks_n)](x, z, group_sz=3)
-
-        # fmt: off
-        expected_z = torch.tensor(
-            [
-                [ 0,  3,  6,  9],
-                [ 1,  4,  7, 10],
-                [ 2,  5,  8, 11],
-                [12, 14, 16, 18],
-                [13, 15, 17, 19],
-            ],
-            device=x.device,
-        )
-        # fmt: on
-        assert torch.equal(z, expected_z)
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.skipif(
+    torch.cuda.is_available() is False, reason="Requires CUDA capable GPU"
+)
+@pytest.mark.parametrize(
+    "num_blocks_m, num_blocks_n, group_size_m, expected_out",
+    [
+        pytest.param(
+            5,
+            4,
+            2,
+            torch.tensor(
+                [
+                    [0, 2, 4, 6],
+                    [1, 3, 5, 7],
+                    [8, 10, 12, 14],
+                    [9, 11, 13, 15],
+                    [16, 17, 18, 19],
+                ],
+                device="cuda",
+            ),
+        ),
+        pytest.param(
+            5,
+            4,
+            3,
+            torch.tensor(
+                [
+                    [0, 3, 6, 9],
+                    [1, 4, 7, 10],
+                    [2, 5, 8, 11],
+                    [12, 14, 16, 18],
+                    [13, 15, 17, 19],
+                ],
+                device="cuda",
+            ),
+        ),
+        pytest.param(
+            5,
+            4,
+            5,
+            torch.tensor(
+                [
+                    [0, 5, 10, 15],
+                    [1, 6, 11, 16],
+                    [2, 7, 12, 17],
+                    [3, 8, 13, 18],
+                    [4, 9, 14, 19],
+                ],
+                device="cuda",
+            ),
+        ),
+    ],
+)
+def test_swizzle(
+    num_blocks_m: int,
+    num_blocks_n: int,
+    group_size_m: int,
+    expected_out: torch.Tensor,
+):
+    x = torch.arange(num_blocks_m * num_blocks_n, device="cuda").view(
+        num_blocks_m, num_blocks_n
+    )
+    out = -torch.ones_like(x)
+    kernel_swizzle[(num_blocks_m, num_blocks_n)](x, out, group_sz=group_size_m)
+    assert torch.equal(out, expected_out)
