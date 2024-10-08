05410e2eb478066d74a6a6e0b5065145bf55d4ad
Daniel Luo <dan.luo1996@gmail.com>
2024-10-08 14:03:35 +0000

    test: use pytest

--- tk/vector_addition_test.py
+++ tk/vector_addition_test.py
@@ -1,15 +1,17 @@
+import pytest
 import torch
 
 from tk.vector_addition import add_vectors
 
 
-def test_add():
+@pytest.mark.parametrize("size", [1, 36, 100, 98432])
+def test_add(size: int):
     torch.manual_seed(0)
-    size = 98432
+
     x = torch.rand(size, device="cuda")
     y = torch.rand(size, device="cuda")
 
-    output_torch = x + y
-    output_triton = add_vectors(x, y)
+    out_torch = x + y
+    out_triton = add_vectors(x, y)
 
-    assert torch.allclose(output_triton, output_torch)
+    assert torch.allclose(out_torch, out_triton)
