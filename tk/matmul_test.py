6a992bdbf5e9379a04d014d967615b09526fda92
Daniel Luo <dan.luo1996@gmail.com>
2024-10-08 16:09:17 +0000

    test: test different group sizes

--- tk/matmul_test.py
+++ tk/matmul_test.py
@@ -13,6 +13,7 @@ from tk.matmul_autotuned import matmul_grouped_autotuned
 @pytest.mark.parametrize(
     "m, k, n", [(8, 16, 8), (3, 4, 5), (30, 40, 50), (300, 400, 500)]
 )
+@pytest.mark.parametrize("group_size_m", [16, 32])
 @pytest.mark.parametrize("kernel", [kernel_matmul_naive, kernel_matmul_grouped])
 @pytest.mark.parametrize(
     "dtype, atol, rtol",
@@ -21,9 +22,18 @@ from tk.matmul_autotuned import matmul_grouped_autotuned
         (torch.float32, 1e-8, 1e-6),
     ],
 )
-def test_matmul(m: int, n: int, k: int, kernel: Callable, dtype, atol, rtol):
+def test_matmul(
+    m: int,
+    n: int,
+    k: int,
+    group_size_m: int,
+    kernel: Callable,
+    dtype: torch.dtype,
+    atol: float,
+    rtol: float,
+):
     a = torch.ones((m, k), dtype=dtype, device="cuda")
     b = torch.ones((k, n), dtype=dtype, device="cuda")
-    out = matmul(a, b, kernel_matmul=kernel, group_size=32)
+    out = matmul(a, b, kernel_matmul=kernel, group_size=group_size_m)
     expected_out = a @ b
     assert torch.allclose(out, expected_out, atol=atol, rtol=rtol)
