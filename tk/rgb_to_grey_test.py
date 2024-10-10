66e23a8a27191ba8a927039ae1cf1c22680b8672
Daniel Luo <dan.luo1996@gmail.com>
2024-10-10 10:27:19 +0000

    test: rgb to grey for different block sizes

--- tk/rgb_to_grey_test.py
+++ tk/rgb_to_grey_test.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import pytest
 import torch
 import torchvision as tv
 import torchvision.transforms.functional as tvf
@@ -8,17 +9,18 @@ from torchvision import io
 from tk.rgb_to_grey import rgb_to_grey
 
 
-def test_rgb_to_grey():
+@pytest.mark.parametrize("block_size", [(16, 16), (32, 32), (64, 64)])
+def test_rgb_to_grey(block_size: tuple[int, int]):
     # Load and resize image
     path_img = pathlib.Path(__file__).parent / "dog.jpg"
     if not path_img.exists():
         raise FileNotFoundError("Dog image not found!")
     img = io.read_image(path_img)
     img = tvf.resize(img, 150, antialias=True)
-    assert img.shape == (3, 150, 225)
+    assert img.shape == (3, 150, 225), "Resized image has incorrect shape!"
     img = img.to("cuda")
 
     # Convert to grey scale
-    grey_img = rgb_to_grey(img, block_size=(32, 32))
+    grey_img = rgb_to_grey(img, block_size=block_size)
     expected_grey_img = tv.transforms.Grayscale()(img)
     assert torch.allclose(grey_img, expected_grey_img)
