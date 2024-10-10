8b91342e31c5edf5a6a81da7cd88d3adaeca99ff
Daniel Luo <dan.luo1996@gmail.com>
2024-10-10 09:54:28 +0000

    feat: fused softmax

--- tk/fused_softmax_test.py
+++ tk/fused_softmax_test.py
@@ -1,11 +1,26 @@
+import pytest
 import torch
 
 from tk.fused_softmax import softmax
 
+torch.manual_seed(0)
 
-def test_softmax():
-    torch.manual_seed(0)
-    x = torch.randn(1823, 781, device="cuda")
-    y_triton = softmax(x)
-    y_torch = torch.softmax(x, axis=1)
-    assert torch.allclose(y_triton, y_torch)
+
+@pytest.mark.skipif(torch.cuda.is_available() is False, reason="Requires CUDA GPU")
+@pytest.mark.parametrize("n_rows, n_cols", [(3, 4), (64, 512), (1823, 781)])
+@pytest.mark.parametrize(
+    "dtype, atol, rtol",
+    [
+        (torch.bfloat16, 1e-10, 0),
+        (torch.float32, 1e-12, 0),
+    ],
+)
+def test_softmax(
+    n_rows: int, n_cols: int, dtype: torch.dtype, atol: float, rtol: float
+):
+    logits = torch.randn((n_rows, n_cols), device="cuda")
+    probs = softmax(logits)
+    assert probs.shape == (n_rows, n_cols)
+
+    expected_probs = torch.softmax(logits, axis=1)
+    assert torch.allclose(probs, expected_probs)
